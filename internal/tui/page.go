71265d807b501e649fd9c17f7b32d68c9ba12ee1
austin-weeks <168379071+austin-weeks@users.noreply.github.com>
2025-11-14 22:57:57 -0500

    add animated loading spinner

--- internal/tui/page.go
+++ internal/tui/page.go
@@ -1,6 +1,9 @@
 package tui
 
 import (
+	"time"
+
+	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/glamour"
@@ -22,28 +25,31 @@ func welcomeScreen(w int, h int) string {
 `)
 }
 
-func loadingScreen(w int, h int) string {
-	return lipgloss.NewStyle().Width(w).Height(h).
-		Foreground(BORDER).AlignHorizontal(lipgloss.Center).AlignVertical(lipgloss.Center).
-		Render(`o o o`)
-}
-
 type page struct {
-	ready     bool
-	viewport  viewport.Model
-	contentFn func(w int, h int) (string, error)
+	ready          bool
+	refreshContent bool
+	viewport       viewport.Model
+	spinner        spinner.Model
+	contentFn      func(w int, h int, p page) (string, error)
 }
 
 func newPage() page {
 	viewport := viewport.New(0, 0)
 	viewport.Style = viewport.Style.Border(lipgloss.RoundedBorder()).BorderForeground(BORDER)
+	spin := spinner.New()
+	spin.Spinner = spinner.Spinner{
+		Frames: []string{"∙∙∙∙∙∙∙", "●∙∙∙∙∙∙", "∙●∙∙∙∙∙", "∙∙●∙∙∙∙", "∙∙∙●∙∙∙", "∙∙∙∙●∙∙", "∙∙∙∙∙●∙", "∙∙∙∙∙∙●"},
+		FPS:    time.Second / 9, //nolint:mnd
+	}
+
 	return page{
 		viewport: viewport,
+		spinner:  spin,
 	}
 }
 
 func (p page) Init() tea.Cmd {
-	return nil
+	return p.spinner.Tick
 }
 
 func (p page) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -55,7 +61,7 @@ func (p page) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		p.viewport.Width, p.viewport.Height = msg.Width, msg.Height
 		if !p.ready {
 			p.ready = true
-			p.setContent(func(w int, h int) (string, error) {
+			p.setContent(func(w int, h int, p page) (string, error) {
 				return welcomeScreen(w-5, h), nil
 			})
 		} else {
@@ -67,19 +73,21 @@ func (p page) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		cmds = append(cmds, cmd)
 
 	case pageContentMsg:
+		p.refreshContent = false
 		if msg.c.Content == "" {
-			p.setContent(func(w int, h int) (string, error) {
+			p.setContent(func(w int, h int, p page) (string, error) {
 				return welcomeScreen(w, h), nil
 			})
 		} else {
-			cmd = p.setContent(func(w int, h int) (string, error) {
+			cmd = p.setContent(func(w int, h int, p page) (string, error) {
 				return renderMarkdown(msg.c.Content, w)
 			})
 			cmds = append(cmds, cmd)
 		}
 
 	case pageErrMsg:
-		p.setContent(func(w int, h int) (string, error) {
+		p.refreshContent = false
+		p.setContent(func(w int, h int, p page) (string, error) {
 			text := "#Something went wrong :(\n\n\n" + msg.err.Error()
 			s, err := renderMarkdown(text, w)
 			if err != nil {
@@ -89,10 +97,17 @@ func (p page) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		})
 
 	case onLoadMsg:
-		p.viewport.SetContent(loadingScreen(p.viewport.Width-4, p.viewport.Height))
+		p.refreshContent = true
+		p.setContent(func(w, h int, p page) (string, error) {
+			s := p.spinner.View()
+			s = lipgloss.Place(w-8, h, lipgloss.Center, lipgloss.Center, s)
+			return s, nil
+		})
 
 	default:
 		p.viewport, cmd = p.viewport.Update(msg)
+		cmds = append(cmds, cmd)
+		p.spinner, cmd = p.spinner.Update(msg)
 		cmds = append(cmds, cmd)
 	}
 
@@ -100,6 +115,9 @@ func (p page) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (p page) View() string {
+	if p.refreshContent {
+		p.setContent(p.contentFn)
+	}
 	return p.viewport.View() + "\n"
 }
 
@@ -114,9 +132,9 @@ func renderMarkdown(content string, w int) (string, error) {
 	return r.Render(content)
 }
 
-func (p *page) setContent(fn func(w int, h int) (string, error)) tea.Cmd {
+func (p *page) setContent(fn func(w int, h int, p page) (string, error)) tea.Cmd {
 	p.contentFn = fn
-	s, err := fn(p.viewport.Width, p.viewport.Height)
+	s, err := fn(p.viewport.Width, p.viewport.Height, *p)
 	if err != nil {
 		return asCmd(pageErrMsg{err: err})
 	}
